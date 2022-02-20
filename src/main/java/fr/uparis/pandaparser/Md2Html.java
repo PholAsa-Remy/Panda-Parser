a92a0a33276a94c8def7bdade7a13b903cda5d65
Pholasa <pholasa.remy@outlook.fr>
2022-02-20 20:56:58 +0100

    [md2html] First Test and First Implementation of the functionality

--- src/main/java/fr/uparis/pandaparser/Md2Html.java
+++ src/main/java/fr/uparis/pandaparser/Md2Html.java
@@ -1,9 +1,36 @@
 package fr.uparis.pandaparser;
+import org.commonmark.node.*;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Md2Html implements IParser{
+    private final org.commonmark.parser.Parser parser = Parser.builder().build();
+
+    private String readFile (String fileName) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = Files.newBufferedReader(Paths.get(fileName));
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        return sb.toString();
+    }
 
     @Override
     public String parse(String fileName) {
+        try {
+            String fileContent = readFile(fileName);
+            Node document = parser.parse(fileContent);
+            HtmlRenderer renderer = HtmlRenderer.builder().build();
+            return renderer.render(document);
+        }catch (IOException e){
+            //Do Something ?
+        }
         return "";
     }
 }
--- src/test/java/fr/uparis/pandaparser/Md2HtmlTest.java
+++ src/test/java/fr/uparis/pandaparser/Md2HtmlTest.java
@@ -2,11 +2,23 @@ package fr.uparis.pandaparser;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Md2HtmlTest {
 
     @Test
-    void parse() {
+    void parseEmptyMarkDown() {
+        Md2Html parser = new Md2Html();
+        URL basicMarkDownRessource = getClass().getClassLoader().getResource("EmptyMarkDown.md");
+        assertEquals("",parser.parse(basicMarkDownRessource.getPath()));
+    }
+
+    @Test
+    void parseBasicMarkDown() {
+        Md2Html parser = new Md2Html();
+        URL basicMarkDownRessource = getClass().getClassLoader().getResource("BasicMarkDown.md");
+        assertEquals("<p>This is a basic markdown</p>\n",parser.parse(basicMarkDownRessource.getPath()));
     }
 }
