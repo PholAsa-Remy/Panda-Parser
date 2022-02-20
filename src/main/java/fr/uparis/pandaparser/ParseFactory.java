f2433070d09524884483514d7344a0bc9b101e03
Pholasa <pholasa.remy@outlook.fr>
2022-02-20 20:59:34 +0100

    [md2html] parseFactory -> ParseFactory

--- src/main/java/fr/uparis/pandaparser/ParseFactory.java
+++ src/main/java/fr/uparis/pandaparser/ParseFactory.java
@@ -1,16 +1,14 @@
 package fr.uparis.pandaparser;
 
-public class parseFactory {
+public class ParseFactory {
 
     public IParser getParser (String parserType){
 
         if(parserType == null)
             return null;
 
-        /* wait for the class Md2Html
         if (parserType.equals(Config.OPTION.get(0)))
             return new Md2Html ();
-        */
         return null;
     }
 }
