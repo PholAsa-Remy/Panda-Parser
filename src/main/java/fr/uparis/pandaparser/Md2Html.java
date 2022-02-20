05d550904f63feda1f77d4c5bfa7ef01c5cbfce4
Pholasa <pholasa.remy@outlook.fr>
2022-02-20 19:46:09 +0100

    [md2html] Create Basics Class for the md2thml parser

---
+++ src/main/java/fr/uparis/pandaparser/Md2Html.java
@@ -0,0 +1,9 @@
+package fr.uparis.pandaparser;
+
+public class Md2Html implements IParser{
+
+    @Override
+    public String parse(String fileName) {
+        return "";
+    }
+}
---
+++ src/test/java/fr/uparis/pandaparser/Md2HtmlTest.java
@@ -0,0 +1,12 @@
+package fr.uparis.pandaparser;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Md2HtmlTest {
+
+    @Test
+    void parse() {
+    }
+}
