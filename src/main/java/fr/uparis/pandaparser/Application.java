a847e30f80a078f0cb7e01e258c37d28b6815024
anesboz <anesboz3@gmail.com>
2022-02-24 14:30:02 +0100

    Thu Feb 24 14:30:01 CET 2022

--- src/main/java/fr/uparis/pandaparser/Application.java
+++ src/main/java/fr/uparis/pandaparser/Application.java
@@ -1,9 +1,9 @@
 package fr.uparis.pandaparser;
 
-import java.util.Arrays;
+import static fr.uparis.pandaparser.CommandManager.triggerCommand;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("hello Panda Parser" + Arrays.toString(args));
+        triggerCommand(args);
     }
 }
