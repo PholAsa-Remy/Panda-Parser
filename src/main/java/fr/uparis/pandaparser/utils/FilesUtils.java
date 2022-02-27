3479dc4b7eb6457d9c41df65bb082878ec95a92d
Lounis BOULDJA <lounisboulja@gmail.com>
2022-02-27 21:36:09 +0100

    read & write file

--- src/main/java/fr/uparis/pandaparser/utils/FilesUtils.java
+++ src/main/java/fr/uparis/pandaparser/utils/FilesUtils.java
@@ -1,16 +1,47 @@
 package fr.uparis.pandaparser.utils;
 
 
+import java.io.*;
 
+/**
+ * FilesUtils class, help methods to manipulate files.
+ *
+ * @author pada-parser group
+ * @version 1.0.0
+ * @since Fev 2022
+ */
 public class FilesUtils {
 
-
-    public final String getFileCotent(final String filePath) {
-        // TODO implement this methode
-        return null;
+    /**
+     * private constructor
+     */
+    private FilesUtils() {
     }
 
-    public void createFileFromContent(final String filename, final String content) {
-        // TODO implement this methode
+
+    /**
+     * Read the content of a file
+     *
+     * @param filePath the file name we want to read
+     * @return fileContent
+     */
+    public static String getFileCotent(final String filePath) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            reader.lines().forEach(line -> sb.append(line).append("\n"));
+        }
+        return sb.substring(0, sb.length() - 1);
+    }
+
+    /**
+     * Create html file from text
+     *
+     * @param filePath file path
+     * @param text     text to write in file
+     */
+    public static void createFileFromContent(final String filePath, final String text) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            writer.write(text);
+        }
     }
 }
