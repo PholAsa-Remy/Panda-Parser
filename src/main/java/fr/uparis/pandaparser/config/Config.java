5402903e2437a07f5d3a6d43d241663cea15a0a4
Lounis BOULDJA <lounisboulja@gmail.com>
2022-02-28 20:14:03 +0100

    Panda parser (simple & full website) architecture

--- src/main/java/fr/uparis/pandaparser/config/Config.java
+++ src/main/java/fr/uparis/pandaparser/config/Config.java
@@ -8,5 +8,7 @@ public class Config {
     public static final String appName = "panda-parser";
     public static int TIMEOUT = 100;
     public static List<String> OPTION = List.of("MD");
+
+    public static final int DEFAULT_MACHINE_JOB = Runtime.getRuntime().availableProcessors();
 }
 
