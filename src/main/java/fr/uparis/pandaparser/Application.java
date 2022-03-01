3c6e777094d1dce078e0f55f86778479d852c74e
Lounis BOULDJA <lounisboulja@gmail.com>
2022-03-02 00:15:07 +0100

    docs

--- src/main/java/fr/uparis/pandaparser/Application.java
+++ src/main/java/fr/uparis/pandaparser/Application.java
@@ -3,6 +3,13 @@ package fr.uparis.pandaparser;
 
 import fr.uparis.pandaparser.core.commande.CommandManager;
 
+/**
+ * Application - Main Class
+ *
+ * @author panda-parser group
+ * @version 1.0.0
+ * @since Fev 2022
+ */
 public class Application {
     /**
      * main method
