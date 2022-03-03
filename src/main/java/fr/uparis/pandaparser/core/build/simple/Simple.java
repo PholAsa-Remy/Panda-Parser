23a3e74f86eb9679ff210a4978293d173e09d8c9
Pholasa <pholasa.remy@outlook.fr>
2022-03-03 12:19:17 +0100

    [simple] Transfering from md2html to Simple.java

--- src/main/java/fr/uparis/pandaparser/core/build/simple/Simple.java
+++ src/main/java/fr/uparis/pandaparser/core/build/simple/Simple.java
@@ -7,10 +7,7 @@ import lombok.extern.java.Log;
 import org.commonmark.node.*;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
-
-import java.io.*;
-
-import org.w3c.tidy.Tidy;
+import java.io.IOException;
 
 import static fr.uparis.pandaparser.utils.FilesUtils.createFileFromContent;
 import static fr.uparis.pandaparser.utils.FilesUtils.getFileContent;
@@ -25,7 +22,7 @@ import static fr.uparis.pandaparser.utils.FilesUtils.getFileContent;
 @Log
 public class Simple extends PandaParser {
 
-    /* An instance of the parser of common-markdown library */
+    /* An instance of the parser of common-markdown library*/
     private final org.commonmark.parser.Parser parser = Parser.builder().build();
 
     public Simple(String input, String output, boolean watch, int jobs) {
@@ -66,29 +63,13 @@ public class Simple extends PandaParser {
         return "<!DOCTYPE html><html>" + header + body + "</html>";
     }
 
-    /**
-     * Make the html more readable
-     * @param htmlContent the content of the html to beautify
-     * @return beautifiedHtml the beautified html
-     */
-    private String beautifyHtml (String htmlContent){
-        Tidy tidy = new Tidy();
-        tidy.setIndentContent(true);
-        tidy.setErrout(null);
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(htmlContent.getBytes());
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        tidy.parse(inputStream,outputStream);
-        return outputStream.toString();
-    }
-
     @Override
     public void parse() {
         try{
-            String inputFileName = FilesUtils.getFileName(this.input);
             String fileContent = getFileContent(input);
-            String html = beautifyHtml(buildHtml(fileContent));
-            createFileFromContent(this.output + inputFileName ,html);
-            log.info("MD 2 HTML parser : input" + this.input + " -> out: " + this.output + inputFileName);
+            String html = buildHtml(fileContent);
+            createFileFromContent(output,html);
+            log.info("MD 2 HTML parser : input" + this.input + " -> out: " + this.output + FilesUtils.getFileName(this.input));
         }catch(IOException e){
             e.printStackTrace();
         }
