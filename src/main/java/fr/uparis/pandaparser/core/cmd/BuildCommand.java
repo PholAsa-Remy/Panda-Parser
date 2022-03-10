49967528f2db27c70ae8487f1983ce84b30428ee
AISSAT Djedjiga <djedjiaissat@gmail.com>
2022-03-10 15:15:45 +0100

    Update help command

--- src/main/java/fr/uparis/pandaparser/core/cmd/BuildCommand.java
+++ src/main/java/fr/uparis/pandaparser/core/cmd/BuildCommand.java
@@ -6,20 +6,20 @@ import picocli.CommandLine.*;
 
 import java.util.concurrent.Callable;
 
-@Command(name = "build", description = "To  parse file from md To html ...", mixinStandardHelpOptions = true)
+@Command(name = "build", description = "Traduction d'un ou plusieurs fichiers Markdown en fichier(s) HTML5", mixinStandardHelpOptions = true)
 public class BuildCommand implements Callable<Integer> {
 
 
-    @Option(names = {"-i", "--input"}, paramLabel = "INPUT_FILE", description = "...")
+    @Option(names = {"-i", "--input-dir"}, paramLabel = "INPUT_DIR", description = "Permet de spécifier une arborescence à traiter différente du répertoire courant.")
     private String input = Config.DEFAULT_INPUT;
 
-    @Option(names = {"-o", "--output"}, paramLabel = "OUTPUT_FILE", description = "...")
+    @Option(names = {"-o", "--output-dir"}, paramLabel = "OUTPUT_DIR", description = "Permet de spécifier où le résultat sera stocké ")
     private String output = Config.DEFAULT_OUTPUT;
 
-    @Option(names = {"-j", "--jobs"}, paramLabel = "NB_JOBS", description = "...")
+    @Option(names = {"-j", "--jobs"}, paramLabel = "NB_JOBS", description = "Désactive l’incrémentalité")
     private Integer jobs = Config.DEFAULT_MACHINE_JOB;
 
-    @Option(names = {"-w", "--watch"}, description = "...")
+    @Option(names = {"-w", "--watch"}, description = "Surveille les fichiers sources et recompile automatiquement le site à chaque modification, avec recalcul")
     private boolean watched;
 
 
