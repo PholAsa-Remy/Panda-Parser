0030ad30f9e0bca0e19405752ee0b21f6d76aa0c
CHHOA thierry <thierry.chhoa@etu.univ-paris-diderot.fr>
2022-03-13 19:03:11 +0100

    [static]: add new directory for the class StaticFile.java

---
+++ src/main/java/fr/uparis/pandaparser/core/staticFile/StaticFile.java
@@ -0,0 +1,22 @@
+package fr.uparis.pandaparser.core.staticFile;
+
+import fr.uparis.pandaparser.config.Config;
+import fr.uparis.pandaparser.utils.FilesUtils;
+
+import java.io.IOException;
+import java.util.Set;
+
+public class StaticFile {
+    /*
+    private Set<String> getAllStaticFiles(String input) throws IOException {
+        String contentDirectoryPath = input + Config.DEFAULT_CONTENT_DIR;
+        return FilesUtils.getAllStaticFilesFromDirectory(contentDirectoryPath);
+    }
+
+    public void setAllStaticFiles(String output, Set<String> staticFiles) throws IOException {
+
+        String contentDirectoryPath = output + "static/";
+
+
+    }*/
+}
