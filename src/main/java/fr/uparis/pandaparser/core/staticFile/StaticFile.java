73aa2fea36153f4dbeecf3153169e3b1fa998fcb
CHHOA thierry <thierry.chhoa@etu.univ-paris-diderot.fr>
2022-03-13 20:04:15 +0100

    [static] : add functions getAllStaticFiles and setAllStaticFiles

--- src/main/java/fr/uparis/pandaparser/core/staticFile/StaticFile.java
+++ src/main/java/fr/uparis/pandaparser/core/staticFile/StaticFile.java
@@ -2,21 +2,40 @@ package fr.uparis.pandaparser.core.staticFile;
 
 import fr.uparis.pandaparser.config.Config;
 import fr.uparis.pandaparser.utils.FilesUtils;
+import lombok.extern.java.Log;
 
 import java.io.IOException;
 import java.util.Set;
 
+@Log
 public class StaticFile {
-    /*
+
+    /**
+     * @param input directory source path
+     * get all static filenames in the directory
+     * @return set Of static filenames
+     */
     private Set<String> getAllStaticFiles(String input) throws IOException {
-        String contentDirectoryPath = input + Config.DEFAULT_CONTENT_DIR;
-        return FilesUtils.getAllStaticFilesFromDirectory(contentDirectoryPath);
+        return FilesUtils.getAllStaticFilesFromDirectory(input);
     }
 
-    public void setAllStaticFiles(String output, Set<String> staticFiles) throws IOException {
-
-        String contentDirectoryPath = output + "static/";
-
-
-    }*/
+    /**
+     * @param input directory source path
+     * @param output directory destination path
+     * copy all the static files from input directory to output directory
+     */
+    public void setAllStaticFiles(String input, String output) throws IOException {
+        String inputDirectoryPath = input + Config.DEFAULT_STATIC_DIR;
+        String outputDirectoryPath = output + Config.DEFAULT_STATIC_DIR;
+        Set<String> staticFiles = getAllStaticFiles(input);
+        staticFiles.forEach((file) -> {
+            try {
+                FilesUtils.copyFileFromInputToOutput( inputDirectoryPath + "/" + file,outputDirectoryPath + "/" + file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        log.info("static : Successful transfer static files from " + inputDirectoryPath
+                + "to " + outputDirectoryPath);
+    }
 }
