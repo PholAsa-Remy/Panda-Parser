db4f918d2fc68240ecce0dcf58e498cef48d08fa
CHHOA thierry <thierry.chhoa@etu.univ-paris-diderot.fr>
2022-03-17 16:46:42 +0100

    [static]: unit test for the extension static

--- src/test/java/fr/uparis/pandaparser/config/TestConfig.java
+++ src/test/java/fr/uparis/pandaparser/config/TestConfig.java
@@ -7,4 +7,7 @@ public class TestConfig {
     public static final String INPUT_TEST_DIR = "src" + File.separator + "test" + File.separator + "resources" + File.separator + "builder-test";
     public static final String OUTPUT_TEST_DIR = "src" + File.separator + "test" + File.separator + "resources" + File.separator + "test-output";
     public static final String MD_FILE = INPUT_TEST_DIR + File.separator + Config.DEFAULT_CONTENT_DIR + Config.DEFAULT_INDEX_NAME;
+    public static final String TEST_MP3_PATH = "/static/01.Ren'ai_Circulation.mp3";
+    public static final String TEST_JPG_PATH = "/static/koharu.jpg";
+    public static final String TEST_CSS_PATH = "/static/exampleTest.css";
 }
