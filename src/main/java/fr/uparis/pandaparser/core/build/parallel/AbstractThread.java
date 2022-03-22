8a0a2dd13abc81084fb5d4a49d09bf6f7a68aff7
Lounis BOULDJA <lounisboulja@gmail.com>
2022-03-22 20:00:49 +0100

    ThreadParser AbstractThread

--- src/main/java/fr/uparis/pandaparser/core/build/parallel/AbstractThread.java
+++ src/main/java/fr/uparis/pandaparser/core/build/parallel/AbstractThread.java
@@ -6,6 +6,12 @@ import lombok.extern.java.Log;
 import java.util.concurrent.Callable;
 
 /**
+ * Abstract Thread to create thread with input & output fields
+ *
+ * <p>
+ * see also: {@link Callable} {@link ThreadParser} {@link ThreadStaticFilesCopier}
+ * </p>
+ *
  * @author panda-parser group
  * @version 1.0.0
  * @since Mars 2022
