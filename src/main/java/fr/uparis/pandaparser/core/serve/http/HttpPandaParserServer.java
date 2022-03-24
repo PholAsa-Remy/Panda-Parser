2c54ccbe683c49d3e34a85956faae2c15490fa22
Lounis BOULDJA <lounisboulja@gmail.com>
2022-03-24 12:05:26 +0100

    init server & ssl verification

--- src/main/java/fr/uparis/pandaparser/core/serve/http/HttpPandaParserServer.java
+++ src/main/java/fr/uparis/pandaparser/core/serve/http/HttpPandaParserServer.java
@@ -1,5 +1,16 @@
 package fr.uparis.pandaparser.core.serve.http;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.SslProvider;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
 import lombok.extern.java.Log;
 
 import javax.net.ssl.SSLException;
@@ -12,5 +23,34 @@ public record HttpPandaParserServer(int port, String output) {
 
     public void start() throws CertificateException, SSLException {
 
+        final SslContext sslCtx;
+        if (SSL) {
+            SelfSignedCertificate ssc = new SelfSignedCertificate();
+            sslCtx = SslContextBuilder.forServer(ssc.certificate(), ssc.privateKey())
+                    .sslProvider(SslProvider.JDK).build();
+        } else
+            sslCtx = null;
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new HttpPandaParserServerInitializer(sslCtx, output));
+
+            Channel ch = b.bind(this.port).sync().channel();
+
+            log.info("Open your web browser and navigate to " + (SSL ? "https" : "http") + "://localhost:" + this.port + '/');
+
+            ch.closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
     }
 }
