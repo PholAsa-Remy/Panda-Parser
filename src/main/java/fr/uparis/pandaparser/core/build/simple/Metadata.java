3903997caaa0232c363bfb151013d27b2323a3e2
CHHOA thierry <thierry.chhoa@etu.univ-paris-diderot.fr>
2022-03-29 00:50:18 +0200

    [template]: update the metadata for the extension template, content key

--- src/main/java/fr/uparis/pandaparser/core/build/simple/Metadata.java
+++ src/main/java/fr/uparis/pandaparser/core/build/simple/Metadata.java
@@ -2,9 +2,13 @@ package fr.uparis.pandaparser.core.build.simple;
 
 import lombok.*;
 import lombok.extern.java.Log;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 import org.tomlj.Toml;
 import org.tomlj.TomlParseResult;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Set;
@@ -22,9 +26,17 @@ import java.util.regex.Pattern;
 @Log
 public class Metadata {
 
+    /* An instance of the parser of common-markdown library */
+    private final org.commonmark.parser.Parser parser = Parser.builder().build();
+
     @Getter
     private HashMap<String,Object> metadata;
 
+    /**
+     * Constructor Metadata, get the list of metadata in the header of the fileContent and
+     * the content under the header of the file
+     * @param fileContent file content
+     */
     public Metadata(String fileContent) {
         metadata = new HashMap<>();
 
@@ -42,8 +54,18 @@ public class Metadata {
 
         while(itr.hasNext()){
             String next = itr.next();
-            metadata.put(next,result.get(next));
+            metadata.put(next, result.get(next).toString());
         }
+        // remove the header from the fileContent
+        Pattern patternHeader = Pattern.compile("(?:\\+{3})((?:.|\\n)*?)(?:\\+{3})");
+        Matcher matchHeader = patternHeader.matcher(fileContent);
+        String fileContentNoHeader = matchHeader.replaceAll("");
+
+        //parse the fileContentNoHeader in html for the key "content"
+        Node document = parser.parse(fileContentNoHeader);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        String newFileContentHTML = renderer.render(document);
+        metadata.put("content", newFileContentHTML);
     }
 
 }
