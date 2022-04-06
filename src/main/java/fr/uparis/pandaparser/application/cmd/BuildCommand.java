dcdb26629455416ad6629cad75cf6459d1284e2c
AISSAT Djedjiga <djedjiaissat@gmail.com>
2022-04-06 17:07:15 +0200

    [incremental] update help feature

--- src/main/java/fr/uparis/pandaparser/application/cmd/BuildCommand.java
+++ src/main/java/fr/uparis/pandaparser/application/cmd/BuildCommand.java
@@ -6,23 +6,23 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import java.util.concurrent.Callable;
 
-@Command(name = "build", description = "Traduction d'un ou plusieurs fichiers Markdown en fichier(s) HTML5", mixinStandardHelpOptions = true)
+@Command(name = "build", description = "Translation of one or more Markdown files into HTML5 file(s)", mixinStandardHelpOptions = true)
 public class BuildCommand implements Callable<Integer> {
 
 
-    @Option(names = {"-i", "--input-dir"}, paramLabel = "INPUT_DIR", description = "Permet de spécifier une arborescence à traiter différente du répertoire courant.")
+    @Option(names = {"-i", "--input-dir"}, paramLabel = "INPUT_DIR", description = "Specify a tree to process different from the current directory.")
     private String input = Config.DEFAULT_INPUT;
 
-    @Option(names = {"-o", "--output-dir"}, paramLabel = "OUTPUT_DIR", description = "Permet de spécifier où le résultat sera stocké ")
+    @Option(names = {"-o", "--output-dir"}, paramLabel = "OUTPUT_DIR", description = "Specify where the result will be stored")
     private String output = Config.DEFAULT_OUTPUT;
 
-    @Option(names = {"-j", "--jobs"}, paramLabel = "NB_JOBS", description = "Désactive l’incrémentalité")
+    @Option(names = {"-j", "--jobs"}, paramLabel = "NB_JOBS", description = "Disable incremental")
     private Integer jobs = Config.DEFAULT_MACHINE_JOB;
 
-    @Option(names = {"-w", "--watch"}, description = "Surveille les fichiers sources et recompile automatiquement le site à chaque modification, avec recalcul")
+    @Option(names = {"-w", "--watch"}, description = "Monitors the source files and automatically recompiles the site at each modification, with recalculation")
     private boolean watched;
 
-    @Option(names = {"--rebuild-all"}, description = "Fichiers modified implique il sera rebuild")
+    @Option(names = {"--rebuild-all"}, description = "All files will be rebuild")
     private boolean rebuildAll;
 
 
