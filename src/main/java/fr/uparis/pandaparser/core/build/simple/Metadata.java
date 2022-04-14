60d92cd82f8a098ea9bcd0380ef0d8dcb98bb831
CHHOA thierry <thierry.chhoa@etu.univ-paris-diderot.fr>
2022-04-14 20:56:51 +0200

    [template++] first implementation template++

--- src/main/java/fr/uparis/pandaparser/core/build/simple/Metadata.java
+++ src/main/java/fr/uparis/pandaparser/core/build/simple/Metadata.java
@@ -3,11 +3,11 @@ package fr.uparis.pandaparser.core.build.simple;
 import lombok.Getter;
 import lombok.extern.java.Log;
 import org.tomlj.Toml;
+import org.tomlj.TomlArray;
 import org.tomlj.TomlParseResult;
+import org.tomlj.TomlTable;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -48,8 +48,41 @@ public class Metadata {
 
         while (itr.hasNext()) {
             String next = itr.next();
-            metadata.put(next, result.get(next));
+            if (result.isArray(next)){
+                metadata.put(next, convertToList((TomlArray) Objects.requireNonNull(result.get(next))));
+            }else if (result.isTable(next)){
+                metadata.put(next, convertToDictionary((TomlTable) Objects.requireNonNull(result.get(next))));
+            }else {
+                metadata.put(next, result.get(next));
+            }
         }
         metadata.put("content", fileContentWithoutHeader);
+    }
+
+    private List<Object> convertToList (TomlArray tomlArray){
+        List<Object> listConvert = tomlArray.toList();
+        for (int i = 0; i < listConvert.size(); i++){
+            if (listConvert.get(i) instanceof TomlTable) {
+                listConvert.set(i, convertToDictionary((TomlTable) listConvert.get(i)));
+            }else if (listConvert.get(i) instanceof TomlArray){
+                listConvert.set(i, convertToList((TomlArray) listConvert.get(i)));
+            }
+        }
+        return listConvert;
+    }
+
+    private Map<String, Object> convertToDictionary (TomlTable tomlTable){
+        Map<String, Object> mapConvert = tomlTable.toMap();
+
+        Set<String> setKeys = mapConvert.keySet();
+        List<String> listKeys = new ArrayList(setKeys);
+        for (int i = 0; i < listKeys.size(); i++){
+            if (mapConvert.get(listKeys.get(i)) instanceof TomlTable){
+                mapConvert.put(listKeys.get(i), convertToDictionary((TomlTable) mapConvert.get(listKeys.get(i))));
+            }else if (mapConvert.get(listKeys.get(i)) instanceof TomlArray){
+                mapConvert.put(listKeys.get(i), convertToList((TomlArray) mapConvert.get(listKeys.get(i))));
+            }
+        }
+        return mapConvert;
     }
 }
